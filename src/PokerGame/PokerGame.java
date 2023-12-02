a1b8b3d45d0d2e56183229e347653d7f85bdbbea
Andrew <andre.ecurut@gmail.com>
2023-12-02 11:46:18 +0400

    Porker

--- src/PokerGame/PokerGame.java
+++ src/PokerGame/PokerGame.java
@@ -1,8 +1,22 @@
 package PokerGame;
 
+import java.util.Scanner;
+
 public class PokerGame {
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        //create a deck object for our deck
+        DeckList deckList = new DeckList();
+
+        //create players of the game.
+        Player player1 = new Player("Andrew");
+        Player player2 = new Player("Esther");
+
+    }
+
+    static void dealIntialCards(DeckList deckList, Player player1, Player player2){
 
     }
 }
